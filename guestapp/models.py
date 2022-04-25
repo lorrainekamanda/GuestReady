cd2059574af0a293c71f49470520231700dab997
Rain <user@localhost.localdomain>
2022-04-25 16:12:07 -0400

    add Rental model

--- guestapp/models.py
+++ guestapp/models.py
@@ -1,5 +1,10 @@
 from django.db import models
+from datetime import datetime
+
+class Rental(models.Model):
+   name = models.CharField(max_length = 200,unique = True)
+
+   def __str__(self):
+       return self.name
 
 
-
-# Create your models here.
